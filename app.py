12c0a0bbdbd07468acf3c10d4be3e96b3f2879a1
josueeng <josue.programador@hotmail.com>
2025-04-04 16:29:06 -0300

    Api Cadastro

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def get_produtos():
     return jsonify(produtos), 200
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use a variável PORT ou padrão 5000
+    app.run(host='0.0.0.0', port=port)
